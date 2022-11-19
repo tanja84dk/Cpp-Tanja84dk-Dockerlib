819c4343a2531f00f490f0c393e4d9e89048c0d6
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2022-11-19 19:37:34 +0000

    Deleted unused files

--- cmake/Findargparse.cmake
+++
@@ -1,5 +0,0 @@
-CPMAddPackage(
-    NAME argparse
-    GIT_REPOSITORY https://github.com/p-ranav/argparse
-    VERSION 2.9
-)
--- cmake/Findcurl.cmake
+++
@@ -1,6 +0,0 @@
-CPMAddPackage(
-    NAME curl
-    GIT_REPOSITORY https://github.com/curl/curl
-    VERSION 7.86.0
-    GIT_TAG curl-7_86_0
-)
