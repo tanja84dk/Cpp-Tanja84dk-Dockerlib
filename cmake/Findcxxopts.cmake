531ee309ed5c1f927a0e7bc045c29ee18d56f6ed
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2022-11-20 02:22:07 +0000

    Updated cmake find cxxopts file

--- cmake/Findcxxopts.cmake
+++ cmake/Findcxxopts.cmake
@@ -2,4 +2,8 @@ CPMAddPackage(
     NAME cxxopts
     GIT_REPOSITORY https://github.com/jarro2783/cxxopts
     VERSION 3.0.0
+    OPTIONS
+        "BUILD_TESTING OFF"
+        "CXXOPTS_BUILD_EXAMPLES OFF"
+        "CXXOPTS_USE_UNICODE_HELP ON"
 )
