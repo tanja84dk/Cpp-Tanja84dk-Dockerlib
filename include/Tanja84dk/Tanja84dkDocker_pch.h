ccbcf400f75581b493bfeea195c094ce0adfa47b
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2022-11-24 12:50:20 +0000

    Moved Config file and deactivated client build when included

--- include/Tanja84dk/Tanja84dkDocker_pch.h
+++ include/Tanja84dk/Tanja84dkDocker_pch.h
@@ -6,6 +6,6 @@
 #include <fmt/core.h>
 #include <asio.hpp>
 #include <nlohmann/json.hpp>
-#include <Tanja84dkDockerConfig.h>
+#include <Tanja84dk/Tanja84dkDockerConfig.h>
 
 #endif // TANJA84DK_DOCKERLIB_DOCKERLIB_PCH_H
