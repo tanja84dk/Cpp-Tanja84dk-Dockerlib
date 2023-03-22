f54a424829203a34b9ba477cec09810c19f54dfd
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-03-22 22:50:27 +0100

    Trying to fix a pendantic warning
    Removed the extra end ; there looks like it gave the error
    src/time.cpp:8:6: error: extra ‘;’ [-Wpedantic]

--- src/time.cpp
+++ src/time.cpp
@@ -5,7 +5,7 @@ namespace Tanja84dk::Time
     std::string unixTimeToHumanReadable(const int64_t &epochTime)
     {
         return unixTimeToHumanReadable(epochTime, "%Y-%m-%d %H:%M:%S");
-    };
+    }
 
     std::string unixTimeToHumanReadable(const int64_t &epochTime, const char *tzFormat)
     {
