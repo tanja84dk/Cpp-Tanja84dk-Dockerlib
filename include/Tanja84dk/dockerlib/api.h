c28a354843be0211940d942b45814182f135dccd
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-06-24 14:12:17 +0000

    refactored restart container

--- include/Tanja84dk/dockerlib/api.h
+++ include/Tanja84dk/dockerlib/api.h
@@ -111,9 +111,13 @@ namespace Tanja84dk::dockerlib::api
       return Client;
     }
 
-    std::string restart(const std::string &container_name_string)
+    ApiRequest restart(const std::string &container_name_string, [[maybe_unused]] const std::string &data = "")
     {
-      return "/containers/" + container_name_string + "/restart";
+      ApiRequest Client;
+      Client.request_type = "POST";
+      Client.url_path = "/containers/" + container_name_string + "/restart";
+      Client.content_type = "application/json";
+      return Client;
     }
 
     std::string kill(const std::string &container_name_string)
