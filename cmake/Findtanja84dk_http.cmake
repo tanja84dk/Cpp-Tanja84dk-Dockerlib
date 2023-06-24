3a4b36f669cd927d06f4c3e7c122a22e619e1b76
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-06-24 19:16:47 +0000

    Updated version because of missing header guard

--- cmake/Findtanja84dk_http.cmake
+++ cmake/Findtanja84dk_http.cmake
@@ -1,6 +1,6 @@
 CPMAddPackage(
     NAME            tanja84dk_http
     GIT_REPOSITORY  https://gitea.home.tanja84.dk/tanja84dk/Cpp-Tanja84dk-HTTPLib
-    GIT_TAG         66b25b860110df5d20d34ba7e0bdded194f098be
+    GIT_TAG         5fec49ec515cb595196b24d1d057d7f7bb2f17b9
     VERSION         0.1.0
 )
