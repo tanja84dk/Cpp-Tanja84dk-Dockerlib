9f4b2308f7d4caf40966a49c408277478e132a5e
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-15 03:21:41 +0000

    made a flag from commandline to print licenses

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -148,13 +148,18 @@ int main(int argc, const char *argv[]) {
                                  "A small scraped docker client from Tanja84dk using web api\n"
                                  "If none arguments are given then you would be able to set it in the program\n");
         options.add_options()("H,Host", "Setting the Host", cxxopts::value<std::string>())(
-            "P,Port", "Setting the Port", cxxopts::value<std::string>())("h,help", "Print Usage");
+            "P,Port", "Setting the Port", cxxopts::value<std::string>())("h,help", "Print Usage")(
+            "l,license", "Printing the licences");
         options.allow_unrecognised_options();
         auto result = options.parse(argc, argv);
         try {
             if (result.count("help")) {
                 fmt::print("{}\n", options.help());
                 exit(0);
+            }
+            if (result.count("license")) {
+                Tanja84dk::dockerlib::license::print_all_licences();
+                return 0;
             }
             if (result.count("Host")) {
                 configuration.set_host(result["Host"].as<std::string>());
