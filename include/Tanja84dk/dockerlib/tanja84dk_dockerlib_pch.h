100bb4dc1bb285859b270c2c5e3f153124562bab
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-07-15 03:21:11 +0000

    added it to the pch header to include

--- include/Tanja84dk/dockerlib/tanja84dk_dockerlib_pch.h
+++ include/Tanja84dk/dockerlib/tanja84dk_dockerlib_pch.h
@@ -1,6 +1,7 @@
 #ifndef TANJA84DK_DOCKERLIB_DOCKERLIB_PCH_H
 #define TANJA84DK_DOCKERLIB_DOCKERLIB_PCH_H
 
+#include <Tanja84dk/dockerlib/license.h>
 #include <Tanja84dk/dockerlib/tanja84dk_dockerlib_config.h>
 #include <fmt/core.h>
 #include <string.h>
