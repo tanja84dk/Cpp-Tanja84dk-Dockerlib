cc853cc965cdce267991f78da6403cc61c329ce9
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-08-27 21:24:02 +0000

    removed the old gtest cmake files since unused

--- cmake/Findgflags.cmake
+++
@@ -1,7 +0,0 @@
-CPMAddPackage(
-    NAME gflags
-    GIT_REPOSITORY https://github.com/gflags/gflags
-    VERSION 2.2.2
-    OPTIONS
-        "BUILD_TESTING OFF"
-)
--- cmake/Findglog.cmake
+++
@@ -1,9 +0,0 @@
-CPMAddPackage(
-  NAME            glog
-  GIT_REPOSITORY  https://github.com/google/glog
-  VERSION         0.7.0
-  GIT_TAG         3a0d4d22c5ae0b9a2216988411cfa6bf860cc372
-  OPTIONS
-    "WITH_GFLAGS OFF"
-    "BUILD_TESTING OFF"
-)
