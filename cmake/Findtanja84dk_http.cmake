653a23506aaa372b4a141cfb5c469b72343e7ad9
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-08-27 21:22:26 +0000

    updated tanja84dk_http library version

--- cmake/Findtanja84dk_http.cmake
+++ cmake/Findtanja84dk_http.cmake
@@ -1,6 +1,6 @@
 CPMAddPackage(
     NAME            tanja84dk_http
     GIT_REPOSITORY  https://gitea.home.tanja84.dk/tanja84dk/Cpp-Tanja84dk-HTTPLib
-    GIT_TAG         f06ea05f4cfb28b1aef7660c69d8a02593605668
+    GIT_TAG         ee3ca9b62f596b7612268bba47cd0c7e19997c5c
     VERSION         0.1.1
 )
