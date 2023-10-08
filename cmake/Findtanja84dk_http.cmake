9bcd17fc356988cd9603d275fe49a0634ff1d7ab
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-10-08 22:41:55 +0000

    Updated to newest nightly

--- cmake/Findtanja84dk_http.cmake
+++ cmake/Findtanja84dk_http.cmake
@@ -1,6 +1,6 @@
 CPMAddPackage(
     NAME            tanja84dk_http
     GIT_REPOSITORY  https://gitea.home.tanja84.dk/tanja84dk/Cpp-Tanja84dk-HTTPLib
-    GIT_TAG         20230830_nightly-v0.1.1
+    GIT_TAG         20231009_nightly_dev
     VERSION         0.1.1
 )
