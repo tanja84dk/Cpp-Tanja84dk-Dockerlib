843e1f94dac8baf13eee9eddbf469017b8de1782
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-10-30 14:24:10 +0000

    added the simple http client

--- client_new/src/main.cpp
+++ client_new/src/main.cpp
@@ -1,1 +1,18 @@
-int main() { return 0; }
+#include <httplib.h>
+
+#include <iostream>
+
+const char* HOST = "127.0.0.1";
+const char* PORT = "80";
+
+int main() {
+    httplib::Client cli(HOST, std::stoi(PORT));
+    if (auto res = cli.Get("/")) {
+        std::cout << "Status: " << res->status << '\n';
+        std::cout << "Content-Type: " << res->get_header_value("Content-Type") << '\n';
+        std::cout << "Body: " << res->body << '\n';
+    } else {
+        std::cerr << "Error: " << res.error() << std::endl;
+        return EXIT_FAILURE;
+    }
+}
