49cd318f37536f5f3a4c3224f34488891426820f
tanja84dk <1285458+tanja84dk@users.noreply.github.com>
2023-10-30 15:01:07 +0000

    Added my simple settings class for docker

--- client_new/src/main.cpp
+++ client_new/src/main.cpp
@@ -7,13 +7,34 @@ const char* HOST = "127.0.0.1";
 const char* PORT = "80";
 
 int main() {
-    httplib::Client cli(HOST, std::stoi(PORT));
-    if (auto res = cli.Get("/")) {
-        std::cout << "Status: " << res->status << '\n';
-        std::cout << "Content-Type: " << res->get_header_value("Content-Type") << '\n';
-        std::cout << "Body: " << res->body << '\n';
-    } else {
-        std::cerr << "Error: " << res.error() << std::endl;
-        return EXIT_FAILURE;
+    auto& configuration = Tanja84dk::dockerlib::ConfigClass::get_instance();
+
+    if (configuration.get_hostname_length() <= 2) {
+        std::string temp_hostname_string;
+        // get_and_validate_input(temp_hostname_string, "Enter the hostname:");
+        if (temp_hostname_string.empty()) {
+            std::cerr << "Missing the hostname or ip\n";
+            return EXIT_FAILURE;
+        };
+        configuration.set_host(temp_hostname_string);
+    }
+
+    if (configuration.get_port_length() <= 1) {
+        std::string temp_port_string;
+        // get_and_validate_input(temp_port_string, "Enter the port (Default 2375): ");
+        if (temp_port_string.empty()) {
+            temp_port_string = "2375";
+        };
+        configuration.set_port(temp_port_string);
+
+        httplib::Client cli(configuration.get_host(), std::stoi(configuration.get_port()));
+        if (auto res = cli.Get("/")) {
+            std::cout << "Status: " << res->status << '\n';
+            std::cout << "Content-Type: " << res->get_header_value("Content-Type") << '\n';
+            std::cout << "Body: " << res->body << '\n';
+        } else {
+            std::cerr << "Error: " << res.error() << std::endl;
+            return EXIT_FAILURE;
+        }
     }
 }
